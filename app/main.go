c84ad4345c4dc5fa8e95c775e617c77e437efc49
George Cheng <george.y.cheng@gmail.com>
2024-04-23 15:19:47 -0400

    update hello

--- app/main.go
+++ app/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Good afternoon, George!")
+		fmt.Fprint(w, "Hello world, George!")
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
